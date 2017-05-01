9a20298738229a3e1f99804e041c1b27e4df3e26
JunkoFujino <kagura.fj@gmail.com>
2017-05-01 07:37:34 +0000

    DIVE09KADAI

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 100.times do |n|
-#  email = Faker::Internet.email
-#  password = "password"
-#  User.create!(email: email,
-#               password: password,
-#               password_confirmation: password,
-#               )
-#end
+ 100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
 
 @users = User.all
 
