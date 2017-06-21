d4a914c594c2ed3e2895dd08a90b3c3db453248a
fujinojunko <fjjnmrjn@ya2.so-net.ne.jp>
2017-06-21 09:54:22 +0900

    DIVE19 AWS

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'achieve'
 set :repo_url, 'https://github.com/junkofujino/achieve'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, ENV['BRANCH'] || 'master'
+set :branch, ENV['BRANCH'] || 'develop'
 
 # deploy先のディレクトリ。
 set :deploy_to, '/var/www/achieve'
